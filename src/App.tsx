8d3bff181e4e32ae431839e6866b31e9bb7c508a
Ivan <ivanteozhiming@gmail.com>
2025-01-17 23:10:27 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className="max-w-[1280px] mx-auto p-8 text-center">
+      <h1>hello, i'm azure</h1>
+      <p>I'm a private chef based in Singapore</p>
+    </div>
+  )
+}
+
+export default App
